03cad15246ef7e3be0e85e85f3ac3accc9e1c705
williameliasson-dev <williameliasson5@gmail.com>
2025-07-14 15:49:00 +0200

    woopsi

--- hosts/lennart/configuration.nix
+++ hosts/lennart/configuration.nix
@@ -23,62 +23,63 @@
       efi.canTouchEfiVariables = false;
       swraid.enable = true;
     };
+  };
 
-    # Networking
-    networking = {
-      hostName = "homelab";
-      networkmanager.enable = true;
-      firewall = {
-        enable = true;
-        # Ports will be opened by individual services
-      };
-    };
-
-    # Users
-    users.users.homelab = {
-      isNormalUser = true;
-      extraGroups = [
-        "wheel"
-        "networkmanager"
-      ];
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLhqTjuSrzwdJoVmJXQlcAXV+I0YJ9Fd/7Di+59sGb0 williameliasson5@gmail.com"
-      ];
-    };
-
-    # SSH
-    services.openssh = {
+  # Networking
+  networking = {
+    hostName = "homelab";
+    networkmanager.enable = true;
+    firewall = {
       enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-      };
+      # Ports will be opened by individual services
     };
+  };
 
-    # System packages
-    environment.systemPackages = with pkgs; [
-      git
-      htop
-      tmux
-      curl
-      wget
-      vim
-      tree
+  # Users
+  users.users.homelab = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLhqTjuSrzwdJoVmJXQlcAXV+I0YJ9Fd/7Di+59sGb0 williameliasson5@gmail.com"
+    ];
+  };
 
-    # Enable flakes
-    nix = {
-      settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      gc = {
-        automatic = true;
-        dates = "weekly";
-        options = "--delete-older-than 30d";
-      };
+  # SSH
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
     };
+  };
 
-    # Time zone
-    time.timeZone = "Europe/Stockholm";
-  }
+  # System packages
+  environment.systemPackages = with pkgs; [
+    git
+    htop
+    tmux
+    curl
+    wget
+    vim
+    tree
+  ];
+
+  # Enable flakes
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  # Time zone
+  time.timeZone = "Europe/Stockholm";
+}
