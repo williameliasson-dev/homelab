5cdbec6805a89df17a260e53524fbd0d1b2624f9
williameliasson-dev <williameliasson5@gmail.com>
2025-09-29 20:17:18 +0200

    bootstrap dns

--- modules/services/blocky.nix
+++ modules/services/blocky.nix
@@ -17,21 +17,27 @@
         http = 4000;
       };
 
-      # Upstream DNS servers (with IPv6 support for Apple devices)
+      # Upstream DNS servers (IPv4 only due to no IPv6 connectivity)
       upstreams = {
         groups = {
           default = [
             "1.1.1.1"
             "1.0.0.1"
-            "2606:4700:4700::1111"
-            "2606:4700:4700::1001"
             "8.8.8.8"
             "8.8.4.4"
-            "2001:4860:4860::8888"
-            "2001:4860:4860::8844"
           ];
         };
       };
+
+      # Bootstrap DNS for downloading blocklists
+      bootstrapDns = [
+        {
+          upstream = "1.1.1.1";
+        }
+        {
+          upstream = "8.8.8.8";
+        }
+      ];
 
       # Custom DNS mappings for local services
       customDNS = {
